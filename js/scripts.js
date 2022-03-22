a8430da3a32cdedb043485163502c9c24f3d6f40
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-22 10:50:45 -0700

    add html, css, js and background image
    co-authored-by: Brady Diamond <brdydmnd@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  
+  $(".clickable").click(function() {
+    $("#animal-hidden").toggle();
+    $("#animal-showing").toggle();
+  });
+});
+
+
+// $(document).ready(function() {
+//   $('.clickable').click(function()  {
+//     $('#walrus-showing').slideToggle();
+//     $('#walrus-hidden').toggle();
+//   });
+// });
+
+
+//https://i.redd.it/3bxixp7u7gk61.png
